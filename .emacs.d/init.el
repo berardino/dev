b8866f88387fae76569ca0cb1ac7353e67c6bffd
Berardino la Torre <bto@tradeshift.com>
2015-05-22 07:58:38 -0700

    add hackernews + projectile everywhere

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,6 +17,7 @@
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
+(setq projectile-require-project-root nil)
 
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
@@ -132,4 +133,5 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+(setq hackernews-top-story-limit 40)
 
